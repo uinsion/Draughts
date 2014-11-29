f6cfab3cbfd1eb1862726264c77cd739e36184a3
FERDI <hoskeee@gmail.com>
2014-11-29 21:39:23 +0000

    Framework 0.2

--- src/DraughtsBoard.java
+++ src/DraughtsBoard.java
@@ -12,6 +12,9 @@ public class DraughtsBoard extends Pane {
 	public DraughtsBoard (){
 		Background = new Rectangle [8][8];
 		
+		initialiseBackground();
+		initialiseRender();
+		resetGame();
 	}
 	
 	@Override
@@ -21,15 +24,14 @@ public class DraughtsBoard extends Pane {
 		cell_width = width / 8.0;
 		cell_height = height / 8.0;
 		
-		createBackground();
+		resizeBackground();
+		pieceResizeRelocate();
 	}
 	
-	public void createBackground(){	
+	public void initialiseBackground(){
 		for(int i = 0; i < 8; i++){
 			for(int j = 0; j < 8; j++){
 				Background[i][j] = new Rectangle();
-				Background[i][j].setWidth(cell_width); Background[i][j].setHeight(cell_height);
-				Background[i][j].relocate(i * cell_width, j * cell_height);
 				
 				if ((i+j) % 2== 0)
 					Background[i][j].setFill(Color.BROWN);
@@ -39,5 +41,69 @@ public class DraughtsBoard extends Pane {
 				this.getChildren().add(Background[i][j]);
 			}
 		}	
+	}
+	
+	public void resizeBackground(){	
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+				Background[i][j].setWidth(cell_width); Background[i][j].setHeight(cell_height);
+				Background[i][j].relocate(i * cell_width, j * cell_height);
+			}
+		}	
+	}
+	
+	private void resetGame(){
+		resetRender();
+		
+		render[1][0].setPiece(1);
+		render[3][0].setPiece(1);
+		render[5][0].setPiece(1);
+		render[7][0].setPiece(1);
+		render[0][1].setPiece(1);
+		render[2][1].setPiece(1);
+		render[4][1].setPiece(1);
+		render[6][1].setPiece(1);
+		render[1][2].setPiece(1);
+		render[3][2].setPiece(1);
+		render[5][2].setPiece(1);
+		render[7][2].setPiece(1);
+		render[0][5].setPiece(2);
+		render[2][5].setPiece(2);
+		render[4][5].setPiece(2);
+		render[6][5].setPiece(2);
+		render[1][6].setPiece(2);
+		render[3][6].setPiece(2);
+		render[5][6].setPiece(2);
+		render[7][6].setPiece(2);
+		render[0][7].setPiece(2);
+		render[2][7].setPiece(2);
+		render[4][7].setPiece(2);
+		render[6][7].setPiece(2);
+		
+	}
+	
+	private void resetRender() {
+		for(int i = 0; i < 8; i++)
+			for(int j = 0; j < 8; j++)
+				render[i][j].setPiece(0); 
+	}
+	
+	private void initialiseRender() {
+
+		render = new DraughtsPiece[8][8]; 
+		for(int i = 0; i < 8; i++)
+			for(int j = 0; j < 8; j++) { 
+				render[i][j] = new DraughtsPiece(0);
+				this.getChildren().add(render[i][j]);
+			}
+	}
+	
+	private void pieceResizeRelocate() {
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+				render[i][j].relocate(i * cell_width, j * cell_height);
+				render[i][j].resize(cell_width, cell_height);
+			}	
+		}
 	}
 }
--- src/DraughtsPiece.java
+++ src/DraughtsPiece.java
@@ -4,9 +4,49 @@ import javafx.scene.shape.Ellipse;
 import javafx.scene.transform.Translate;
 
 //class definition for a reversi piece
-class ReversiPiece extends Group {
+class DraughtsPiece extends Group {
 	
-	public ReversiPiece(int player) {
+	private Ellipse piece;
+	private Translate t;
+	private int player;	
+	
+	public DraughtsPiece(int player) {
+		this.player = player;
+		t = new Translate();
 		
+		piece = new Ellipse();
+		getChildren().addAll(piece);
+		piece.getTransforms().add(t);
+		
+		piece.setFill(Color.TRANSPARENT);
+
 	}
+	
+	@Override
+	public void resize(double width, double height) {
+		super.resize(width, height); 
+		
+		piece.setCenterX(width / 2); piece.setCenterY(height / 2);
+		piece.setRadiusX(width / 2); piece.setRadiusY(height / 2);
+
+	}
+	
+	@Override
+	public void relocate(double x, double y) {
+		super.relocate(x, y); 
+		t.setX(x); t.setY(y);
+	}
+	
+	public void setPiece(final int type) {
+		switch (type){
+		case 0: piece.setFill(Color.TRANSPARENT);
+				break;
+		case 1: piece.setFill(Color.RED);
+				break;
+		case 2: piece.setFill(Color.GREEN);
+				break;
+		}
+			
+	}
+	
 }
