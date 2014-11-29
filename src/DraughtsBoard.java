f36d55e54c4dae201bdc8b1c7db128759a69ed64
FERDI <hoskeee@gmail.com>
2014-11-29 17:28:33 +0000

    FRamwork 0.1.5

--- src/DraughtsBoard.java
+++ src/DraughtsBoard.java
@@ -1,10 +1,8 @@
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.transform.Translate;
 
-public class DraughtsBoard {
+public class DraughtsBoard extends Pane {
 	
 	private Rectangle [][] Background;
 	private double cell_width;
@@ -13,7 +11,6 @@ public class DraughtsBoard {
 	public DraughtsBoard (){
 		Background = new Rectangle [8][8];
 		
-		createBackground();
 	}
 	
 	@Override
@@ -23,12 +20,22 @@ public class DraughtsBoard {
 		cell_width = width / 8.0;
 		cell_height = height / 8.0;
 		
+		createBackground();
 	}
 	
 	public void createBackground(){	
 		for(int i = 0; i < 8; i++){
 			for(int j = 0; j < 8; j++){
+				Background[i][j] = new Rectangle();
+				Background[i][j].setWidth(cell_width); Background[i][j].setHeight(cell_height);
+				Background[i][j].relocate(i * cell_width, j * cell_height);
 				
+				if ((i+j) % 2== 0)
+					Background[i][j].setFill(Color.BROWN);
+				else
+					Background[i][j].setFill(Color.BEIGE);
+				
+				this.getChildren().add(Background[i][j]);
 			}
 		}	
 	}
